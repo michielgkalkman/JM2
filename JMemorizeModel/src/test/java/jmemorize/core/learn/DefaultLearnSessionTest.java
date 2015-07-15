07a5b9ee7e16159ba5272e5b48a517a0be920a93
Michiel G. Kalkman <m>
2015-07-15 15:00:15 +0200

    Improve mutation test coverage

--- JMemorizeModel/src/test/java/jmemorize/core/learn/DefaultLearnSessionTest.java
+++ JMemorizeModel/src/test/java/jmemorize/core/learn/DefaultLearnSessionTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Test;
+
 import jmemorize.core.Card;
 import jmemorize.core.Category;
 import jmemorize.core.Lesson;
@@ -17,8 +19,6 @@ import jmemorize.core.learn.LearnHistory.CalendarComparator;
 import jmemorize.core.learn.LearnHistory.SessionSummary;
 import jmemorize.provider.DefaultLearnSessionProviderImpl;
 import jmemorize.provider.LearnLessonsSessionProvider;
-
-import org.junit.Test;
 
 public class DefaultLearnSessionTest {
 	@Test
@@ -67,8 +67,7 @@ public class DefaultLearnSessionTest {
 			}
 		});
 
-		learnLessonsSessionProvider.startLearnSession(settings, selectedCards,
-				category, learnUnlearned, learnExpired);
+		learnLessonsSessionProvider.startLearnSession(settings, selectedCards, category, learnUnlearned, learnExpired);
 
 		observer.assertObserver(false, false, true, false);
 
@@ -79,8 +78,7 @@ public class DefaultLearnSessionTest {
 		final Lesson lesson = new Lesson(true);
 		final Card card = new Card("0", "0");
 
-		final LearnLessonsSessionProvider learnLessonsSessionProvider = learnStuff(
-				lesson, card);
+		final LearnLessonsSessionProvider learnLessonsSessionProvider = learnStuff(lesson, card);
 
 		learnLessonsSessionProvider.setLesson(lesson);
 
@@ -91,8 +89,7 @@ public class DefaultLearnSessionTest {
 		learnStuff(lesson2, card);
 	}
 
-	private LearnLessonsSessionProvider learnStuff(final Lesson lesson,
-			final Card card) {
+	private LearnLessonsSessionProvider learnStuff(final Lesson lesson, final Card card) {
 		final Category category = lesson.getRootCategory();
 		category.addCard(card);
 
@@ -106,8 +103,7 @@ public class DefaultLearnSessionTest {
 
 		learnLessonsSessionProvider.setLesson(lesson);
 
-		learnLessonsSessionProvider.startLearnSession(settings, selectedCards,
-				category, learnUnlearned, learnExpired);
+		learnLessonsSessionProvider.startLearnSession(settings, selectedCards, category, learnUnlearned, learnExpired);
 
 		assertTrue(learnLessonsSessionProvider.isSessionRunning());
 
@@ -129,52 +125,111 @@ public class DefaultLearnSessionTest {
 		assertSummaries(learnHistory, LearnHistory.WEEK_COMP);
 		assertSummaries(learnHistory, LearnHistory.YEAR_COMP);
 
-		final List<SessionSummary> summaries = learnHistory.getSummaries(
-				LearnHistory.SIMPLE_COMP, 12, true);
+		final List<SessionSummary> summaries = learnHistory.getSummaries(LearnHistory.SIMPLE_COMP, 12, true);
 		assertEquals(0, summaries.size());
-		assertTrue(learnHistory.getSummaries(LearnHistory.SIMPLE_COMP, 12,
-				false).isEmpty());
-		assertTrue(learnHistory.getSummaries(LearnHistory.SIMPLE_COMP)
-				.isEmpty());
+		assertTrue(learnHistory.getSummaries(LearnHistory.SIMPLE_COMP, 12, false).isEmpty());
+		assertTrue(learnHistory.getSummaries(LearnHistory.SIMPLE_COMP).isEmpty());
 		return learnLessonsSessionProvider;
 	}
 
-	private void assertSummaries(final LearnHistory learnHistory,
-			final CalendarComparator calendarComparator) {
+	private void assertSummaries(final LearnHistory learnHistory, final CalendarComparator calendarComparator) {
 		assertTrue(learnHistory.getSummaries(calendarComparator).isEmpty());
-		assertTrue(learnHistory.getSummaries(calendarComparator, 12, false)
-				.isEmpty());
+		assertTrue(learnHistory.getSummaries(calendarComparator, 12, false).isEmpty());
 
-		final List<SessionSummary> summaries = learnHistory.getSummaries(
-				calendarComparator, 12, true);
+		final List<SessionSummary> summaries = learnHistory.getSummaries(calendarComparator, 12, true);
 		assertEquals(3, summaries.size());
-		assertEquals(0.0f, summaries.get(0).getFailed(),
-				Float.POSITIVE_INFINITY);
-		assertEquals(0.0f, summaries.get(0).getPassed(),
-				Float.POSITIVE_INFINITY);
+		assertEquals(0.0f, summaries.get(0).getFailed(), Float.POSITIVE_INFINITY);
+		assertEquals(0.0f, summaries.get(0).getPassed(), Float.POSITIVE_INFINITY);
 		assertEquals(0, summaries.get(0).getDuration());
 	}
-	
+
 	@Test
 	public void testCompare() {
-		Category category = new Category("SampleCatagory");
-		LearnSettings settings = new LearnSettings();
-		List<Card> selectedCards = new ArrayList<Card>();
+		final Category category = new Category("SampleCatagory");
+		final LearnSettings settings = new LearnSettings();
+		final List<Card> selectedCards = new ArrayList<Card>();
 		{
-			Card card = new Card("front", "back");
+			final Card card = new Card("front", "back");
 			category.addCard(card, 1);
 			selectedCards.add(card);
 		}
 		{
-			Card card = new Card("front2", "back2");
+			final Card card = new Card("front2", "back2");
 			category.addCard(card, 2);
 			selectedCards.add(card);
 		}
-		boolean learnUnlearned = true;
-		boolean learnExpired = true;
-		LearnSessionProvider provider = new DefaultLearnSessionProviderImpl();
-		DefaultLearnSession defaultLearnSession = new DefaultLearnSession(category, settings, selectedCards, learnUnlearned, learnExpired, provider);
-		
+		final boolean learnUnlearned = true;
+		final boolean learnExpired = true;
+		final LearnSessionProvider provider = new DefaultLearnSessionProviderImpl();
+		final DefaultLearnSession defaultLearnSession = new DefaultLearnSession(category, settings, selectedCards,
+				learnUnlearned, learnExpired, provider);
+
 		assertNotNull(defaultLearnSession);
 	}
+
+	@Test
+	public void testSkipCard() {
+		final Lesson lesson = new Lesson(true);
+
+		final Category category = lesson.getRootCategory();
+
+		final Card card = new Card("0", "0");
+		category.addCard(card);
+
+		final LearnSettings settings = new LearnSettings();
+
+		final List<Card> selectedCards = new ArrayList<>();
+		selectedCards.add(card);
+
+		final boolean learnUnlearned = true;
+		final boolean learnExpired = true;
+		final LearnLessonsSessionProvider learnLessonsSessionProvider = new DefaultLearnSessionProviderImpl();
+
+		final MockLessonObserver observer = new MockLessonObserver();
+		learnLessonsSessionProvider.addLessonObserver(observer);
+
+		learnLessonsSessionProvider.setLesson(lesson);
+
+		final TestLearnSessionObserver observer2 = new TestLearnSessionObserver();
+
+		learnLessonsSessionProvider.addLearnSessionObserver(observer2);
+
+		learnLessonsSessionProvider.startLearnSession(settings, selectedCards, category, learnUnlearned, learnExpired);
+
+		final LearnSession session = observer2.getSession();
+		assertNotNull(session);
+
+		assertEquals(1, session.getCardsLeft().size());
+		assertEquals(0, session.getSkippedCards().size());
+
+		session.cardSkipped();
+
+		assertEquals(1, session.getCardsLeft().size());
+		assertEquals(1, session.getSkippedCards().size());
+	}
+
+	public static class TestLearnSessionObserver implements LearnSessionObserver {
+
+		private LearnSession session;
+
+		@Override
+		public void sessionStarted(final LearnSession session) {
+			this.setSession(session);
+		}
+
+		@Override
+		public void sessionEnded(final LearnSession session) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public LearnSession getSession() {
+			return session;
+		}
+
+		public void setSession(final LearnSession session) {
+			this.session = session;
+		}
+	}
+
 }
